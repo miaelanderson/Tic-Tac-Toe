f81044fc159eb129c8c31d75686d81e0b6f712c7
Mia <miaelanderson@gmail.com>
2024-11-15 10:04:21 -0600

    Added winner detection routine

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = Array(repeating: "", count: 9)
+    @State private var xTurn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
     var body: some View {
         VStack {
             Text("Tic Tac Toe")
@@ -22,21 +26,21 @@ struct ContentView: View {
                         Text(moves[index])
                             .font(.system(size: 60))
                             .fontWeight(.heavy)
+                    }
+                    .frame(width: 120, height: 120, alignment: .center)
+                    .cornerRadius(30)
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            if moves[index] == "" {
+                                moves[index] = xTurn ? "X" : "O"
+                                xTurn.toggle()
                             }
-                            .frame(width: 120, height: 120, alignment: .center)
-                            .cornerRadius(30)
-                            .onTapGesture {
-                                withAnimation(.default) {
-                                    if moves[index] == "" {
-                                        moves[index] = xTurn ? "X" : "O"
-                                        xTurn.toggle()
-                                    }
-                                }
-                            }
-                            .rotation3DEffect(
-                                .degrees(moves[index] == "" ? 180 : 0),
-                                axis: (x: 0.0, y: 1.0, z: 0.0)
-                                )
+                        }
+                    }
+                    .rotation3DEffect(
+                        .degrees(moves[index] == "" ? 180 : 0),
+                        axis: (x: 0.0, y: 1.0, z: 0.0)
+                    )
                 }
             })
         }
@@ -45,18 +49,19 @@ struct ContentView: View {
             Alert(title: Text(winMessage))
         }
         .onChange(of: moves) { oldValue, newValue in
-        checkForWinner()
+            checkForWinner()
+            
         }
     }
-    @State private var moves = Array(repeating: "", count: 9)
-    @State private var xTurn = true
-    @State private var gameOver = false
-    @State private var winMessage = ""
-    private func checkForWinner() {
-        if moves[0] != "" && moves[0] == moves[1] && moves[1] == moves[2] {
-            winMessage = "\(moves[0]) is the winner!"
+    private func checkLine(a: Int, b: Int, c: Int) {
+        if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
+            winMessage = "\(moves[a]) is the winner!"
             gameOver = true
+            
         }
+    }
+    private func checkForWinner() {
+        checkLine(a: 0, b: 1, c: 2)
     }
 }
 
